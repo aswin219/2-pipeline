6d5623c9dc0ff9863a95b4c371f8f2e30d09ef13
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-13 19:35:38 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,4 +6,4 @@ echo $PASS >> /tmp/.auth
 #
 scp -i /opt/id_rsa /tmp/.auth ec2-user@65.1.130.186:/tmp/.auth 
 scp -i /opt/id_rsa ./jenkins/deploy/publish ec2-user@65.1.130.186:/tmp/publish 
-ssh -i /opt/id_rsa ec2-user@65.1.130.186"/tmp/publish" 
+ssh -i /opt/id_rsa ec2-user@65.1.130.186 "/tmp/publish" 
