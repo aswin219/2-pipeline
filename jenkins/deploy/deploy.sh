1c00e3411e2dd2cb19440838898e283fb6525ea5
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-14 09:57:57 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -3,7 +3,7 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-#
+
 scp -i /opt/id_rsa /tmp/.auth ec2-user@3.108.249.112:/tmp/.auth 
 scp -i /opt/id_rsa ./jenkins/deploy/publish ec2-user@3.108.249.112:/tmp/publish 
 ssh -i /opt/id_rsa ec2-user@3.108.249.112 "/tmp/publish" 
