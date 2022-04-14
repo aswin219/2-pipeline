3276ec40351c116e7c308396dcd0f34fb8684960
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-14 12:32:26 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -o StrictHostKeyChecking=no -i /opt/id_rsa /tmp/.auth ec2-user@3.108.249.112:/tmp/.auth 
-scp -o StrictHostKeyChecking=no -i /opt/id_rsa ./jenkins/deploy/publish ec2-user@3.108.249.112:/tmp/publish 
-ssh -o StrictHostKeyChecking=no -i /opt/id_rsa ec2-user@3.108.249.112 "/tmp/publish" 
+scp -o StrictHostKeyChecking=no /tmp/.auth ec2-user@3.108.249.112:/tmp/.auth 
+scp -o StrictHostKeyChecking=no ./jenkins/deploy/publish ec2-user@3.108.249.112:/tmp/publish 
+ssh -o StrictHostKeyChecking=no ec2-user@3.108.249.112 "/tmp/publish" 
